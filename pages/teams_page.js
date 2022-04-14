be4f915f4fd1f571a90ffc2435ce518f3784a12e
oumaima rih <rihamina04@gmail.com>
2022-04-14 17:24:01 +0000

    delete_a_team

--- pages/teams_page.js
+++ pages/teams_page.js
@@ -7,8 +7,11 @@ module.exports = {
       createButton: 'Create Team',
       submitButton: 'Create Team',
       refreshButton: 'refresh',
+      editButton: '#root > div > div > div.css-jfo9gg > div.sc-dkQkyq.sc-jivBlf.fDiXhU.cSPwSG > div > div.jss12 > div > div > div > table > tbody > tr:nth-child(1) > td:nth-child(9) > div > div:nth-child(1) > label > button > span.MuiButton-label > span > svg > path:nth-child(1)',
       name: '#name',
       teamTypeButton: '//*[@id="chakra-modal--body-6"]/form/div/div[3]/label[2]/span[2]',
+      deleteButton: '#root > div > div > div.css-jfo9gg > div.sc-dkQkyq.sc-jivBlf.fDiXhU.cSPwSG > div > div.jss372 > div > div > div > table > tbody > tr:nth-child(1) > td:nth-child(9) > div > div:nth-child(2) > label > button > span.MuiButton-label > span > svg' ,
+      bouttonDeLaConfirmationDeSuppression: 'body > div.jss570 > div.jss571.jss572 > div > div.sc-jIkXHa.eVSPhG > div:nth-child(2)',
     },
   
     creation_team_pass(teams_url) {
@@ -58,6 +61,19 @@ module.exports = {
       I.click(this.fields.submitButton);
       
     },
+
+      editer_une_team(teams_url){
+      I.say('editer une team');
+      I.amOnPage(teams_url);
+      I.click(this.fields.editButton);
+  },
+      delete_a_team(teams_url){
+        I.say('delete une team');
+        I.amOnPage(teams_url)
+        I.click(this.fields.deleteButton);
+        I.click(this.fields.bouttonDeLaConfirmationDeSuppression);
+        I.dontSeeElement(this.fields.bouttonDeLaConfirmationDeSuppression);
+      }
 
   }
   
